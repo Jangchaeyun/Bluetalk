c4c174648730559268bef520a9d12ec6ee936be8
zzangsally <62280276+Jangchaeyun@users.noreply.github.com>
2022-10-28 17:30:49 +0900

    ADD: Client-Server Communication

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
-import Cookie from 'universal-cookie';
+import Cookies from 'universal-cookie';
 import axios from 'axios';
 
 import signinImage from '../assets/signup.jpg';
+
+const cookies = new Cookies();
 
 const initialState = {
   fullName: '',
@@ -19,14 +21,31 @@ const Auth = () => {
   
   const handleChange = (e) => {
     setForm({ ...form, [e.target.name]: e.target.value });
-
-    console.log(form);
   }
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
-    console.log(form);
+    const { userName, password, phoneNumber, avatarURL } = form;
+
+    const URL = 'http://localhost:5000/auth';
+
+    const { data: { token, userId, hashedPassword, fullName } } = await axios.post(`${URL}/${isSignup ? 'signup': 'login'}`, { 
+      userName, password, fullName: form.fullName, phoneNumber, avatarURL,
+    });
+
+    cookies.set('token', token);
+    cookies.set('userName', userName);
+    cookies.set('fullName', fullName);
+    cookies.set('userId', userId);
+
+    if(isSignup) {
+      cookies.set('phoneNumber', phoneNumber);
+      cookies.set('avatarURL', avatarURL);
+      cookies.set('hashedPassword', hashedPassword);
+    }
+
+    window.location.reload();
   }
 
   const switchMode = () => {
